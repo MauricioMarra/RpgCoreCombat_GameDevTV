bc0b2fc198691d04434da1a850cd46fd385fb387
MauricioMarra <52969819+MauricioMarra@users.noreply.github.com>
2024-05-26 20:34:25 -0300

    feat: implements action priority.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -19,40 +19,54 @@ namespace RPG.Control
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                MoveToCursor();
-                Attack();
-            }
+            if (Attack()) return;
+            if (MoveToCursor()) return;
+
+            Debug.Log("Nothing to do.");
         }
 
-        private void MoveToCursor()
+        private bool MoveToCursor()
         {
             var lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red, 5);
+            //Debug.DrawRay(lastRay.origin, lastRay.direction * 100, Color.red, 5);
 
             RaycastHit hit;
             var hasHit = Physics.Raycast(lastRay, out hit);
 
             if (hasHit)
             {
-                movementController.MoveTo(hit.point);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    movementController.MoveTo(hit.point);
+                }
+
+                return true;
             }
+
+            return false;
         }
 
-        private void Attack()
+        private bool Attack()
         {
             var hits = Physics.RaycastAll(Camera.main.ScreenPointToRay(Input.mousePosition));
 
             foreach (var hit in hits)
             {
-                var target = new CombatTarget();
-                if (hit.transform.TryGetComponent<CombatTarget>(out target))
+                CombatTarget target;
+                target = hit.transform.GetComponent<CombatTarget>();
+
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
                 {
                     fighterController.Attack();
                 }
+
+                return true;
             }
+
+            return false;
         }
     }
 }
